58bfdd0a831193c429509fd4e809f094dc3a6e9e
Sky <m7miaji@gmail.com>
2022-02-23 16:15:08 +0300

    ddd

--- Algorithm/rssScript.py
+++ Algorithm/rssScript.py
@@ -6,21 +6,37 @@ import feedparser
 import webbrowser
 
 # The feedparser that will get the application from the web 
-feed = feedparser.parse("https://finance.yahoo.com/rss/")
+feed = feedparser.parse("https://www.cnbc.com/id/20409666/device/rss/rss.html?x=1")
 
 # This is where every article will be read
-feed_entries = feed.entries
-count = 0
-for entry in feed.entries:
-    count = count + 1
-    article_title = entry.title
-    article_link = entry.link
-    article_published_at = entry.published 
-    article_published_at_parsed = entry.published_parsed
+def feedparser():
+    feed_entries = feed.entries
+    count = 0
+    arr =output = [[0 for i in range(3)] for j in range(10)]
 
-    print(count, "- {}\n[{}]".format(article_title, article_link))
-    print("Published at {}\n".format(article_published_at))
+    for entry in feed.entries:
+        count = count + 1
+        article_title = entry.title
+        article_link = entry.link
+        article_published_at = entry.published 
+        article_published_at_parsed = entry.published_parsed
+        article_summary = entry.description
+        arr[count-1][0]=article_title 
+        arr[count-1][1]=article_published_at
+        arr[count-1][2]=article_link
+        print(article_summary)
+        if count == 10:
+            break
+    data = {"title": arr}
+    return data
+    #print(count, "- {}\n[{}]".format(article_title, article_link))
+    #print("Published at {}\n".format(article_published_at))
 
+data = feedparser()
+'''data1 = {"title": [["kyle", "Jan 1", "1999"],
+                  ["kyle", "Jan 1", "1999"],
+                   ["kyle", "Jan 1", "1999"]]}'''
+#print(data)
 # This will be used to make the access to the application much easier
 """
 def feedparser(id, title, summary, link):
