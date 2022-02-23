fe6c3f3a32715fd48256411112ddbc349b4b973a
Sky <50262300+M7Miaji@users.noreply.github.com>
2022-02-23 16:13:28 +0500

    delete comments

--- apps/home/views.py
+++ apps/home/views.py
@@ -61,15 +61,6 @@ def feedparser():
     feed_entries = feed.entries
     count = 0
     arr =output = [[0 for i in range(4)] for j in range(10)]
-    
-    '''array = [1,2,3,4,5,6,7,8,9,10]
-    array1 = [1,2,3,4,5,6,7,8,9,10]
-    array2 = [1,2,3,4,5,6,7,8,9,10]'''
-
-    '''data = {"title": [1,2,3,4,5,6,7,8,9,10],
-            "date": [1,2,3,4,5,6,7,8,9,10],
-            "link": [1,2,3,4,5,6,7,8,9,10]
-            }'''
 
     for entry in feed.entries:
         count = count + 1
@@ -77,9 +68,6 @@ def feedparser():
         article_link = entry.link
         article_published_at = entry.published 
         article_description = entry.description
-        '''array[count-1] = article_title 
-        array1[count-1] = article_published_at
-        array2[count-1] = article_link'''
         arr[count-1][0]=article_title 
         arr[count-1][1]=article_published_at
         arr[count-1][2]=article_link
@@ -87,9 +75,6 @@ def feedparser():
         if count == 10:
             break
 
-    '''data = {'title': array,
-            'date': array1,
-            'link': array2}'''
     data = {"title": arr}
     return data
 
