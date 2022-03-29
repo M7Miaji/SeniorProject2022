b48afba4131f1c8d580054363e0078ccb34a4e16
KhaledDot <khaledmosel@gmail.com>
2022-03-29 17:29:26 +0300

    creating first table

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,18 +1,15 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
-import email
-from pyexpat import model
 from django.db import models
-from django.contrib.auth.models import User
+
 
 # Create your models here.
-
-Class Members(models.Model):
-    fname = models.CharField(max_length=40)
+class Users(models.Model):
+    username = models.CharField(max_length=40)
     email = models.EmailField(max_length=100)
-    pwd = models.charField(max_length=40)
-    favstocks=models.TextField(max_length=15000)
+    pwd = models.CharField(max_length=40)
+    favstocks= models.CharField(max_length=15000)
+ 
+
+    
+    
 
