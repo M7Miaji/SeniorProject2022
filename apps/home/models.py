8ed72aa85aa29e99540e4250e53389c789948c98
KhaledDot <khaledmosel@gmail.com>
2022-03-31 16:34:16 +0300

    models correction

--- apps/home/models.py
+++ apps/home/models.py
@@ -3,13 +3,17 @@ from django.db import models
 
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=40)
     email = models.EmailField(max_length=100)
     pwd = models.CharField(max_length=40)
     favstocks= models.CharField(max_length=15000)
  
 
-    
+
+class Stock(models.Model):
+   StockName=models.CharField(max_length=100)
+   stockprice=models.DecimalField()
+
     
 
