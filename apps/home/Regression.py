ce326d630e020da0d18aae52791ed78767941da3
Sky <50262300+M7Miaji@users.noreply.github.com>
2022-04-25 20:15:55 +0500

    sum

---
+++ apps/home/Regression.py
@@ -0,0 +1,71 @@
+# imports
+import pandas as pd
+import numpy as np
+
+# read dataset
+filename = "datasets/500_Person_Gender_Height_Weight_Index"
+df = pd.read_csv(f"{filename}.csv", usecols=[1,2,3], header=0, names=["height", "weight", "index"])
+print(df)
+
+# useful functions
+def split_train_test(df, p):
+    """
+    Takes a dataframe and the number of data in the train set.
+    Returns a list of dataframes.
+    """
+    n = int(p*len(df))
+    train = df.iloc[0:n, :]
+    test = df.iloc[n:len(df), :]
+    return train, test
+
+# split dataset into train and test
+train_df, test_df = split_train_test(df, 0.5)
+
+print(train_df)
+print(test_df)
+
+# create array-like objects for train and test data
+x_train = np.array(train_df["height"])
+y_train = np.array(train_df["weight"])
+z_train = np.array(train_df["index"])
+
+x_test = np.array(test_df["height"])
+y_test = np.array(test_df["weight"])
+z_test = np.array(test_df["index"])
+
+# set initial values for learnable parameters
+a = 1
+b = 1
+c = 0
+
+lr = 0.000005 # learning rate
+epochs = 1000 # number of iterations
+
+n = len(z_train)
+
+for i in range(epochs):
+    z_predicted = a*x_train + b*y_train + c # make a prediction
+    error = z_predicted - z_train # calculate the error
+    loss = np.sum(error**2)/n # calculate the loss 
+    loss_a = 2*np.sum(error*x_train)/n # partial derivatives of the loss
+    loss_b = 2*np.sum(error*y_train)/n
+    loss_c = 2*np.sum(error)/n
+    a = a - loss_a*lr # adjust the parameters 
+    b = b - loss_b*lr
+    c = c - loss_c*lr
+    print(f"loss: {loss}  \t({i+1}/{epochs})")
+    
+
+
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+
+from sklearn.metrics import r2_score
+
+z_prediction = a*x_test + b*y_test + c
+print(f"R2 Score: {r2_score(z_test, z_prediction)}")
+
+for i in range(len(z_prediction)):
+    print(f"BMI: {z_test[i]}  Predicted: {z_prediction[i]}")
+
--- apps/home/main_lstm.py
+++ apps/home/main_lstm.py
@@ -1,38 +1,142 @@
-from turtle import forward, st
-from typing import Type
 from matplotlib.pyplot import get
-import numpy as np #The Numpy numerical computing library
-import pandas as pd #The Pandas data science library
-from IPython.display import display
 from scraping import stock, history
-from list import sample
-from statistics import mean
-from nsepy import get_history
-from datetime import datetime
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-import time
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+#from tensorflow import random
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import tensorflow as tf
+import keras
+from keras import optimizers
+from keras.callbacks import History
+from keras.models import Model
+from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
+import time
 
+tf.random.set_seed(20)
+np.random.seed(10)
 np.set_printoptions(suppress=True)
 
-startDate='2020-1-1'
 
-get_data=history(stock('AAPL'), startDate)
+'''
+lstm_input = Input(shape=(data_set_points, 1), name='lstm_input')
+inputs = LSTM(21, name='first_layer', return_sequences = True)(lstm_input)
+inputs = Dropout(0.1, name='first_dropout_layer')(inputs)
+inputs = LSTM(64, name='lstm_1')(inputs)
+inputs = Dropout(0.05, name='lstm_dropout_1')(inputs)
+inputs = Dense(32, name='first_dense_layer')(inputs)
+inputs = Dense(1, name='dense_layer')(inputs)
+output = Activation('linear', name='output')(inputs)
+model = Model(inputs=lstm_input, outputs=output)
+adam = optimizers.Adam(lr = 0.002)
+model.compile(optimizer=adam, loss='mse')
+model.fit(x=X_train, y=y_train, batch_size=15, epochs=25, shuffle=True, validation_split = 0.1)
+'''
 
-get_data['TradeDate']=get_data.index
+def processing(Data):
+    sc=MinMaxScaler()
+    DataScaler=sc.fit(Data)
+    X=DataScaler.transform(Data)
+    X=X.reshape(X.shape[0],)
 
-ClosePrice=get_data[['Close']].values
+    X_samples=list()
+    y_samples=list()
 
-df=pd.DataFrame()
+    NumberofRows=len(X)
+    TimeSteps=10
+    FutureTimeSteps=5
 
-data_columns=[
-    'Open',
-    'Close',
-    'High',
-    'Low'
-]
+    for i in range(TimeSteps, NumberofRows-FutureTimeSteps, 1):
+        x_sample=X[i-TimeSteps:i]
+        y_sample=X[i:i+FutureTimeSteps]
+        X_samples.append(x_sample)
+        y_samples.append(y_sample)
 
-print(type(get_data))
+    X_data=np.array(X_samples)
+    X_data=X_data.reshape(X_data.shape[0],X_data.shape[1],1)
+
+    y_data=np.array(y_samples)
+
+    TestingRecords=5
+
+    X_train=X_data[:-TestingRecords]
+    X_test=X_data[-TestingRecords:]
+    y_train=y_data[:-TestingRecords]
+    y_test=y_data[-TestingRecords:]
+
+    for inp, out in zip(X_train[0:2], y_train[0:2]):
+        print(inp)
+        print('====>')
+        print(out)
+        print('#'*20)
+
+    TimeSteps=X_train.shape[1]
+    TotalFeatures=X_train.shape[2]
+    
+    return TimeSteps, TotalFeatures, FutureTimeSteps, X_train, y_train, X_test, y_test, DataScaler
+
+
+def model(TimeSteps, TotalFeatures, FutureTimeSteps):
+
+    regressor=Sequential()
+    regressor.add(LSTM(units = 10, activation = 'relu', input_shape = (TimeSteps, TotalFeatures), return_sequences=True))
+    regressor.add(LSTM(units = 5, activation = 'relu', input_shape = (TimeSteps, TotalFeatures), return_sequences=True))
+    regressor.add(LSTM(units = 5, activation = 'relu', return_sequences=False ))
+    regressor.add(Dense(units = FutureTimeSteps))
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+    return regressor
+
+def fit(X_train, y_train, regressor):
+    StartTime=time.time()
+
+    regressor.fit(X_train, y_train, batch_size = 5, epochs = 100)
+
+    EndTime=time.time()
+
+    print("############### Total Time Taken: ", round((EndTime-StartTime)/60), 'Minutes #############')
+
+    return regressor
+
+def prediction(regressor, X_test, DataScaler, y_test):
+    predicted_Price = regressor.predict(X_test)
+    predicted_Price = DataScaler.inverse_transform(predicted_Price)
+
+    orig=y_test
+    orig=DataScaler.inverse_transform(y_test)
+
+    return predicted_Price, orig
+
+def main():
+
+    startDate='2020-1-1'
+
+    get_data=history(stock('AAPL'), startDate)
+
+    get_data['TradeDate']=get_data.index
+
+    data=get_data[['Close']].values
+
+    df=pd.DataFrame()
+
+    TimeSteps, TotalFeatures, FutureTimeSteps, X_train, y_train, X_test, y_test, DataScaler = processing(data)
+
+    regressor = model(TimeSteps, TotalFeatures, FutureTimeSteps)
+
+    regressor = fit(X_train, y_train, regressor)
+
+    predicted_Price, orig = prediction(regressor, X_test, DataScaler, y_test)
+
+    print(orig, predicted_Price)
+    for i in range(len(orig)):
+        Prediction=predicted_Price[i]
+        Original=orig[i]
+    print(str(100 - (100*(abs(Original-Prediction)/Original)).mean().round(2)))
+main()
+
+
+
+
+
