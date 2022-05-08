1e1e38e6864fb71e363730fa6b909ebb23789c4e
Sky <m7miaji@gmail.com>
2022-05-08 17:51:16 +0300

    update

--- apps/home/trade.py
+++ apps/home/trade.py
@@ -198,6 +198,55 @@ def predict_future(model, data, DataScaler):
     Next5DaysPrice = DataScaler.inverse_transform(Next5DaysPrice)
     print(Next5DaysPrice)
 
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a= dataset[i:(i+time_step), 0]
+        dataX.append(a)
+        dataY.append(dataset[i+ time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+def regression_model():
+    
+    startDate='2020-1-1'
+
+    df=history(stock('AAPL'), startDate)
+
+    df['TradeDate']=df.index
+    
+    df1 = df[['Close']].values
+
+    df1 = np.array(df1)
+    df1 = df1.reshape(-1,1)
+
+    scaler = MinMaxScaler(feature_range=(0,1))
+    df1 = scaler.fit_transform(df1)
+
+    # splitting dataset into train and test split
+    training_size = int(len(df1)*0.65)
+    test_size = len(df1)-training_size
+    train_data,test_data  = df1[0:training_size,:], df1[training_size:len(df1),:1]
+
+    time_step = 100
+    X_train, y_train = create_dataset(train_data, time_step)
+    X_test, y_test = create_dataset(test_data, time_step)
+    train_data.shape, test_data.shape
+
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    print(predictions)
+    print("Predicted Value",predictions[:10][0])
+    print("Expected Value",y_test[:10][0])
+
+    pred_df= pd.DataFrame(predictions)
+    pred_df['TrueValues']=y_test
+
+    new_pred_df=pred_df.rename(columns={0: 'Predictions'})
+    print(new_pred_df.head())
+
 def main():
     startDate='2010-1-1'
     get_data=history(stock('AAPL'), startDate)
